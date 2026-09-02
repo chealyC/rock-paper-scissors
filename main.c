101897a238fd16c8df1e60c77ce178e276fde6e4
Christian <xianhealy@gmail.com>
2026-09-02 10:28:44 +0100

    Update main.c

--- main.c
+++ main.c
@@ -5,12 +5,15 @@
 
 int main(void) 
 {
+    /*Seed the random number generator using the current time*/
     srand(time(NULL));
 
+    /*Initialise scores*/
     int playerScore = 0;
     int computerScore = 0;
     int draws = 0;
 
+    /*Start game*/
     char playAgain = 'y';
 
     while(playAgain == 'y' || playAgain == 'Y')
@@ -26,6 +29,7 @@ int main(void)
 
         int result = determineWinner(player, computer);
 
+        /*Update score based on result*/
         if (result == 0)
         {
             printf("It's a draw!\n");
@@ -42,11 +46,13 @@ int main(void)
             computerScore++;
         }
 
+        /*Display current scores*/
         printf("\nScore:\n");
         printf("Player: %d\n", playerScore);
         printf("Computer: %d\n", computerScore);
         printf("Draws: %d\n", draws);
 
+        /*Confirm if player wants to go another round*/
         printf("\nPress y to play again.\n");
         scanf(" %c", &playAgain);
     }
