1b7d8fabe9b30ee6bc58e925296c1b013353b723
Christian <xianhealy@gmail.com>
2026-09-02 10:31:13 +0100

    Fix comments formatting in game.c

--- game.c
+++ game.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Generate a random hand between 0 and NUM_HANDS - 1*/
+/* Generate a random hand between 0 and NUM_HANDS - 1 */
 int randomHand(void)
 {
     return rand() % NUM_HANDS;
@@ -21,7 +21,7 @@ int chooseHand(void)
 
     scanf("%d", &choice);
 
-    /*Check that the player entered a valid hand*/
+    /* Check that the player entered a valid hand */
     if (choice < 0 || choice > 2)
         printf("Please enter a valid number.\n");
 
@@ -32,7 +32,7 @@ int chooseHand(void)
 
 void displayHand(int hand)
 {
-    /*Display the name of the selected hand*/
+    /* Display the name of the selected hand */
     switch (hand)
     {
         case 0:
@@ -57,11 +57,11 @@ void displayHand(int hand)
 */
 int determineWinner(int player, int computer)
 {
-    /*Same hand means a draw*/
+    /* Same hand means a draw */
     if (player == computer)
         return 0;
 
-    /*Checks possible combinations where the player wins*/
+    /* Checks possible combinations where the player wins */
     if ((player == 0 && computer == 1) ||
         (player == 1 && computer == 2) ||
         (player == 2 && computer == 0))
